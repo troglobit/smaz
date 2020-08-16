07fd96d5f00600ec3a924042a9cd4296064ee8ac
Joachim Nilsson <troglobit@gmail.com>
2020-08-16 13:47:06 +0200

    smaz: dynamic memory allocation and further cleanup
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- main.c
+++ main.c
@@ -1,12 +1,11 @@
+#include <errno.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include "smaz.h"
 
-#define MAXSIZE 4096
-
-static const char *prognm;
+static char *prognm;
 
 int usage(int rc) {
     FILE *fp = rc ? stderr : stdout;
@@ -23,21 +22,15 @@ int usage(int rc) {
     return rc;
 }
 
-int main(int argc, char const *argv[]) {
-    char in[MAXSIZE];
-    char out[MAXSIZE];
-    const char *ptr;
-    FILE *fp_in;
-    FILE *fp_out;
-    int file_size;
-    int output_size;
+int main(int argc, char *argv[]) {
+    char *ibuf, *obuf;
+    int rc = 0, len;
+    size_t isz, osz;
+    FILE *in, *out;
 
-    ptr = strrchr(argv[0], '/');
-    if (ptr)
-        ptr++;
-    else
-        ptr = argv[0];
-    prognm = ptr;
+    prognm = strrchr(argv[0], '/');
+    if (prognm) prognm++;
+    else        prognm = argv[0];
 
     if (!strncmp(argv[1], "--help", 6) || !strncmp(argv[1], "-h", 2))
         return usage(0);
@@ -51,36 +44,61 @@ int main(int argc, char const *argv[]) {
         return usage(1);
     }
         
-
-    fp_in = fopen(argv[2], "rb");
-    if (!fp_in) {
-        fprintf(stderr, "Failed opening input file %s: %m\n", argv[2]);
+    in = fopen(argv[2], "rb");
+    if (!in) {
+        fprintf(stderr, "Failed opening input file %s: %s\n", argv[2], strerror(errno));
         return 1;
     }
         
-    fp_out = fopen(argv[3], "wb");
-    if (!fp_out) {
-        fclose(fp_in);
-        fprintf(stderr, "Failed opening output file %s: %m\n", argv[3]);
+    out = fopen(argv[3], "wb");
+    if (!out) {
+        fclose(in);
+        fprintf(stderr, "Failed opening output file %s: %s\n", argv[3], strerror(errno));
         return 1;
     }
 
-    fseek(fp_in, 0, SEEK_END);
-    file_size = ftell(fp_in);
-    fseek(fp_in, 0, SEEK_SET);
+    fseek(in, 0, SEEK_END);
+    isz = ftell(in);
+    fseek(in, 0, SEEK_SET);
+    ibuf = malloc(isz);
+    if (!ibuf) {
+    memerr:
+        fprintf(stderr, "Failed allocating memory: %s", strerror(errno));
+        rc = 1;
+        goto end;
+    }
 
-    fread(in, file_size, 1, fp_in);
+    osz = isz * 10;
+    obuf = malloc(osz);
+    if (!obuf) {
+        free(ibuf);
+        goto memerr;
+    }
+
+    if (fread(ibuf, isz, 1, in) != isz) {
+        fprintf(stderr, "Failed reading %s: %s", argv[2], strerror(errno));
+        rc = 1;
+        goto readerr;
+    }
 
     if (strncmp(argv[1], "-c", 2) == 0)
-        output_size = smaz_compress(in, file_size, out, MAXSIZE);
+        len = smaz_compress(ibuf, isz, obuf, osz);
     else
-        output_size = smaz_decompress(in, file_size, out, MAXSIZE);
+        len = smaz_decompress(ibuf, isz, obuf, osz);
 
-    fwrite(out, output_size, 1, fp_out);
-    fclose(fp_in);
-    fclose(fp_out);
+    if (fwrite(obuf, len, 1, out) != (size_t)len) {
+        fprintf(stderr, "Failed writing data to %s: %s", argv[3], strerror(errno));
+        rc = 1;
+    }
 
-    return 0;
+readerr:
+    free(ibuf);
+    free(obuf);
+end:
+    fclose(in);
+    fclose(out);
+
+    return rc;
 }
 
 /**
